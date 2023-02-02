8b0659f5a550c0bcfeca276f840a528895a10cc7
JUNGCHAEYOON <108243290+JUNGCHAEYOON@users.noreply.github.com>
2023-02-02 15:03:25 +0900

    0202
    이미지 불러오기 구현

--- app/src/main/java/com/example/risingcamp2week/MAIN/MainActivity.kt
+++ app/src/main/java/com/example/risingcamp2week/MAIN/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.risingcamp2week.MAIN
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.example.risingcamp2week.FRAGMENT.*
 import com.example.risingcamp2week.R
@@ -36,6 +38,10 @@ class MainActivity : AppCompatActivity() {
         * 바텀 네비게이션 및 툴바의 사용은 onCreate 에서 진행
         * 화면이 전환되거나, NewActivity 로 넘어가더라도 다시 onRestart 되어 동기화 되는 것이 아님
         * */
+        /*툴바 설정 */
+//        val toolbar : Toolbar = findViewById(R.layout.toolbar)
+//        setSupportActionBar(toolbar)
+
         /* 바텀네비 바인딩 */
         binding.bottomNavigationView.setOnItemSelectedListener {
                 item -> changeFragment(
@@ -74,6 +80,7 @@ class MainActivity : AppCompatActivity() {
                 binding.fab1.setIconResource(R.drawable.ic_action_plus)
                 binding.fab1.setBackgroundColor(Color.parseColor("#ff8f12"))
                 binding.fab1.setText("글쓰기")
+                binding.fab1.setIconTintResource(R.color.white)
                 binding.fab1.extend()
             }
             clicked = !clicked
@@ -85,10 +92,17 @@ class MainActivity : AppCompatActivity() {
                 binding.fab2.visibility = View.VISIBLE
             }
         }
+
+        /* NewActivity로 전환되는 글쓰기 버튼 fab2 */
+       binding.fab2.setOnClickListener {
+            val intent = Intent(this, NewActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
         super.onStart()
+        // 홈 프래그먼트로 귀환시 중고거래 글쓰기가 추가되었을경우 업데이트
     }
 
     override fun onResume() {
--- app/src/main/java/com/example/risingcamp2week/MAIN/NewActivity.kt
+++ app/src/main/java/com/example/risingcamp2week/MAIN/NewActivity.kt
@@ -1,20 +1,81 @@
 package com.example.risingcamp2week.MAIN
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.risingcamp2week.databinding.ActivityMainBinding
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.risingcamp2week.databinding.ActivityNewBinding
 
 class NewActivity : AppCompatActivity() {
-
     //뷰 바인딩
     private lateinit var binding : ActivityNewBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //뷰 바인딩으로 setContentView
+        /* 뷰 바인딩으로 setContentView */
         binding = ActivityNewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /* shared preferences 로 글제목, 글내용, 이미지 저장 */
+
+
+        /* 툴바 설정 */
+        var toolbar : Toolbar = binding.newtoolbar
+        setSupportActionBar(toolbar)
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+
+        /* 이미지 불러오기 */
+        val getAction = registerForActivityResult(
+            ActivityResultContracts.GetContent(),
+            ActivityResultCallback { uri ->
+                //이미지 선택 완료시 화면에 맞게 크기 조절!
+                binding.imageView.setImageURI(uri)
+                binding.imageView.layoutParams.width= 1020
+                binding.imageView.layoutParams.height= 1020
+            }
+        )
+        binding.imageView.setOnClickListener{
+            getAction.launch("image/*")
+        }
+
+        /* 작성글 homefragment 로 전달 shared preferences */
+
+        /* 툴바 내부 완료 버튼 클릭으로 글쓰기 */
+        binding.finishButton.setOnClickListener{
+            Toast.makeText(this, "작성 완료!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // 글을 쓰다가 뒤로 갔을때, 완료버튼 미클릭 시
+        // 해당 작성 내용이 그대로 유지됨
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
